9c475615e31c63569bc446c84d0fba0de036ab47
Rratic <Rratic@users.noreply.github.com>
2022-08-21 00:51:48 +0000

    deploy: 15ab89804753a46e92c8ad668dec7b8951fa8a23

--- extra/info.js
+++ extra/info.js
@@ -1,8 +1,8 @@
-const buildmessage=`built at Sun Aug 14 08:48:42 2022`
+const buildmessage=`built at Sun Aug 21 00:51:43 2022`
 const page_foot=`Powered by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> and its dependencies.`
 const tar_css=`css`
 const filesuffix=`.html`
-const menu=['',[`misc/杂项`,`fav_scpcn/scp-cn 好文推荐`,`queen/皇后棋`,],]
+const menu=['',[`std/标准化`,`naming/命名规范`,],[`misc/杂项`,`fav_scpcn/scp-cn 好文推荐`,`queen/皇后棋`,`turtlesoup_1/海龟汤归档`,],]
 const configpaths=        {'headroom': 'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.10.3/headroom.min',
 		'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min',
 		'headroom-jquery': 'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.10.3/jQuery.headroom.min',
