12ab80a1ffcd993b32befb623e7031e10cd57e7d
Rratic <Rratic@users.noreply.github.com>
2023-01-01 10:02:52 +0000

    deploy: 3ed5bdc2f2e2e1b3a6f2c2f8349faaf2c3cf49a9

--- extra/info.js
+++ extra/info.js
@@ -1,9 +1,9 @@
 const __lang=`zh`
-const buildmessage=`built at Mon Dec 26 13:55:05 2022 by DoctreePages.jl v1.6.1`
+const buildmessage=`built at Sun Jan  1 10:02:43 2023 by DoctreePages.jl v1.6.1`
 const page_foot=`Powered by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> and its dependencies.`
 const tar_css=`css`
 const filesuffix=`.html`
-const menu=['',`introduction/欢迎`,[`blog/博客`,`done/所做的`,],[`std/标准化`,`naming/命名规范`,],[`misc/杂项`,`fav_scpcn/scp-cn 好文推荐`,`queen/皇后棋`,`turtlesoup_1/海龟汤归档`,],]
+const menu=['',`introduction/主页`,[`blog/博客`,`done/所做的`,`deal/交易`,],[`std/标准化`,`naming/命名规范`,],[`misc/杂项`,`fav_scpcn/scp-cn 好文推荐`,`queen/皇后棋`,`turtlesoup_1/海龟汤归档`,],]
 const configpaths={'headroom':'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.10.3/headroom.min','jquery':'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min','headroom-jquery':'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.10.3/jQuery.headroom.min','katex': 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.4/katex.min','highlight':'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min','hljs-julia':'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/languages/julia.min','hljs-julia-repl':'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/languages/julia-repl.min','hljs-line-numbers':'https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min'}
 const configshim={"hljs-julia-repl":{"deps":["highlight"]},"hljs-julia":{"deps":["highlight"]},"hljs-line-numbers":{"deps":["highlight"]},"headroom-jquery":{"deps":["jquery","headroom"]}}
 const hljs_languages=['julia', 'julia-repl']
