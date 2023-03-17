d354e24b8be4154430eef505c4bfe6554587ce22
Rratic <Rratic@users.noreply.github.com>
2023-03-17 13:28:29 +0000

    deploy: 5fc9f753fbbe6cf7b49f03751a8e4c821d49f43a

--- extra/info.js
+++ extra/info.js
@@ -1,5 +1,5 @@
 const __lang=`zh`
-const buildmessage=`<p>built at Thu Feb  9 04:03:10 2023, by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> v1.8.2</p>`
+const buildmessage=`<p>built at Fri Mar 17 13:28:23 2023, by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> v1.8.2</p>`
 const page_foot=`Powered by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> and its dependencies.`
 const tar_css=`css`
 const filesuffix=`.html`
--- script/queen_main.js
+++ script/queen_main.js
@@ -41,7 +41,7 @@ function showmove(step, fx, fy, x, y){
 }
 
 // 工具函数
-const notify = (str) => window.alert("🔔 "+str)
+const notify = (str) => window.alert(str)
 const loadmo = (e) => new Pos(e.offsetX>>6, e.offsetY>>6)
 const tellwiner = (num) => notify([ "红方食用了绿方", "绿方食用了红方", "红方无路可走", "绿方无路可走" ][num-1])
 const changeorder = () => { current=3-current; showinfo() }
